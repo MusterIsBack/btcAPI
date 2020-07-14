d90df8196b44e5b5ed5c244796eb82425c3bd976
MusterIsBack <68179820+MusterIsBack@users.noreply.github.com>
2020-07-14 20:35:12 +0430

    Update crypto price.py

--- crypto price.py
+++ crypto price.py
@@ -8,13 +8,13 @@ while(True):
         print('The current price is: ' ,price['last'])
         print('The lowest price in the previous 24 hours is: ' ,price['low'])
         print('The highest price in the previous 24 hours is: ' ,price['high'])
-        ques = input('coninue? (y/n)')
+        ques = input('continue? (y/n)')
         if ques == 'y':
             continue
         else:
             break
     else:
-        print ('should be 3 words like BTC or ETH. not accepted BITCOIN or else ...')
+        print ('should be 3 words like BTC or ETH. not accepted BITCOIN or else.')
         break
 
 
